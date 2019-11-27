b8f40739385884296602d9c105ed8badabb95b36
Vasilijs <vasilykom@gmail.com>
2019-11-28 01:12:41 +0200

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,24 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 export interface Post {
   title: string;
   text: string;
   id?: number;
 }
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-posts: Post[] = [
-  { title: 'учу', text: 'англар'},
-  { title: 'учу 2й день', text: 'передача параметров'}
-];
+  posts: Post[] = [
+    {title: 'учу', text: 'англар'},
+    {title: 'учу 2й день', text: 'передача параметров'}
+  ];
 
-updatePost(post) {
-  this.posts.unshift(post)
-  console.log('post app', post);
-}
+  updatePost(post) {
+    this.posts.unshift(post);
+    console.log('post app', post);
+  }
 }
